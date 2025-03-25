5c2f17eca1b00b4e7632fd617f58db7b8afa9cb7
Lokesh Reddy <lokeshreddy1218@gmail.com>
2025-03-25 20:48:58 +0530

    Iniial commit

---
+++ src/main/java/com/smarterReconcilation/smarterReconcilation/SmarterReconcilationApplication.java
@@ -0,0 +1,13 @@
+package com.smarterReconcilation.smarterReconcilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarterReconcilationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarterReconcilationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarterReconcilation/smarterReconcilation/SmarterReconcilationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarterReconcilation.smarterReconcilation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarterReconcilationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
